c6111445ab604fafdc9d3de9b1f4f6d893fb65e7
A-Novychenko <A.Novychenko@outlook.com>
2025-08-28 18:37:03 +0300

    fix favicon met

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,28 +1,3 @@
-// import { Outlet } from 'react-router-dom';
-// import { Suspense } from 'react';
-
-// import { AppBar } from './base';
-// // import { Container } from './ui';
-
-// export const DashboardLayout = () => {
-//   return (
-//     // <div style={{ height: '100vh', marginTop: 64 }}>
-//     // <div style={{ height: 'calc(100vh - 128px)', marginTop: 64 }}>
-//     <div>
-//       <AppBar />
-
-//       {/* <Container> */}
-//       <main style={{ height: 'calc(100vh - 64px)', marginTop: 64 }}>
-//         <Suspense fallback={null}>
-//           <Outlet />
-//         </Suspense>
-//       </main>
-//       {/* </Container> */}
-//     </div>
-//   );
-// };
-
-// src/components/DashboardLayout.tsx
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AppBar } from './base';
